ad93b0f7adf4b13d28db3d22e605c037a0bc16be
Seán Fitzgerald <sean.fitzgerald@hmhco.com>
2020-08-24 12:19:23 +0100

    Logging

--- HomeCooking.Api/Program.cs
+++ HomeCooking.Api/Program.cs
@@ -2,7 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HomeCooking.Data;
+using HomeCooking.Domain.Entities;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -11,8 +14,18 @@ namespace HomeCooking.Api
 {
     public class Program
     {
+        private static RecipeContext _recipeContext = new RecipeContext();
+        
         public static void Main(string[] args)
         {
+            // _recipeContext.Database.EnsureCreated();
+            // _recipeContext.Users.Add(new User()
+            // {
+            //     Name = "Sean"
+            // });
+            // _recipeContext.SaveChanges();
+            // var users = _recipeContext.Users.ToList();
+            //var x = _recipeContext.Recipes.Where(recipe => EF.Functions.Like)
             CreateHostBuilder(args).Build().Run();
         }
 
--- HomeCooking.Data/RecipeContext.cs
+++ HomeCooking.Data/RecipeContext.cs
@@ -1,5 +1,6 @@
 using HomeCooking.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace HomeCooking.Data
 {
@@ -9,9 +10,20 @@ namespace HomeCooking.Data
         public DbSet<Recipe> Recipes { get; set; }
         public DbSet<Ingredient> Ingredients { get; set; }
 
+        public static readonly ILoggerFactory ConsoleLoggerFactory
+            = LoggerFactory.Create(builder =>
+            {
+                builder.AddFilter((category, level) =>
+                            category == DbLoggerCategory.Database.Command.Name && 
+                            level == LogLevel.Information)
+                    .AddConsole();
+            });
+        
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlite($"Data Source= RecipeDb.db");
+            optionsBuilder
+                .UseLoggerFactory(ConsoleLoggerFactory)
+                .UseSqlite($"Data Source= RecipeDb.db");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
