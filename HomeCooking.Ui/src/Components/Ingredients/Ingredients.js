5f006aed7a8ddf6b6499bb39027e98dca3cf4da1
Seán Fitzgerald <seanfitzg@gmail.com>
2021-05-05 17:10:56 +0100

    Add ingredients code.

--- HomeCooking.Ui/src/Components/Ingredients/Ingredients.js
+++ HomeCooking.Ui/src/Components/Ingredients/Ingredients.js
@@ -1,6 +1,11 @@
 import * as React from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { DataGrid } from '@material-ui/data-grid';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles((theme) => ({
   title: {
@@ -8,6 +13,19 @@ const useStyles = makeStyles((theme) => ({
   },
   container: {
     width: '100%',
+  },
+  ingredientHeader: {
+    marginTop: 15,
+    marginBottom: 10,
+  },
+  textBoxes: {
+    '& > *': {
+      marginLeft: 10,
+      width: '25ch',
+    },
+  },
+  deleteButton: {
+    marginTop: 10,
   },
 }));
 
@@ -23,20 +41,85 @@ const rows = [
 
 export default function Ingredients() {
   const classes = useStyles();
+  const [ingredient, setIngredient] = useState('');
+  const [amount, setAmount] = useState('');
+  const [deleteDisabled, setDeleteDisabled] = useState(true);
+  const [ingredientList, setIngredientList] = useState(rows);
+  const [itemsToDelete, setItemsToDelete] = useState([]);
+  const handleAdd = () => {
+    if (!ingredient) return;
+    setIngredientList([
+      ...ingredientList,
+      { id: uuidv4(), ingredient, amount },
+    ]);
+    setAmount('');
+    setIngredient('');
+  };
+
+  const handleDelete = () => {
+    var remaining = ingredientList.filter(
+      (el) => !itemsToDelete.includes(el.id)
+    );
+    setIngredientList(remaining);
+    setDeleteDisabled(true);
+  };
+
+  const checkBoxSelection = (item) => {
+    console.log(`item`, item);
+    setDeleteDisabled(item.selectionModel?.length > 0 ? false : true);
+    setItemsToDelete(item.selectionModel);
+  };
+
   return (
-    <div className={classes.container}>
-      <div className={classes.title}>Ingredients</div>
-      <div style={{ width: '100%' }}>
-        <DataGrid
-          rows={rows}
-          columns={columns}
-          pageSize={100}
-          checkboxSelection
-          autoHeight
-          autoPageSize
-          hideFooterPagination
-        />
+    <>
+      <div className={classes.container}>
+        <Grid
+          className={classes.ingredientHeader}
+          container
+          direction="row"
+          justify="space-between"
+          alignItems="center"
+        >
+          <div className={classes.title}>Ingredients</div>
+          <div className={classes.textBoxes}>
+            <TextField
+              id="ingredient"
+              label="Ingredient"
+              value={ingredient}
+              onChange={(event) => setIngredient(event.target.value)}
+            />
+            <TextField
+              id="amount"
+              label="Amount"
+              value={amount}
+              onChange={(event) => setAmount(event.target.value)}
+            />
+            <Button onClick={handleAdd} variant="contained">
+              Add Ingredient
+            </Button>
+          </div>
+        </Grid>
+        <div style={{ width: '100%' }}>
+          <DataGrid
+            rows={ingredientList}
+            columns={columns}
+            pageSize={100}
+            checkboxSelection
+            autoHeight
+            autoPageSize
+            hideFooterPagination
+            onSelectionModelChange={(item) => checkBoxSelection(item)}
+          />
+        </div>
+        <Button
+          disabled={deleteDisabled}
+          onClick={handleDelete}
+          variant="contained"
+          className={classes.deleteButton}
+        >
+          Delete Ingredients
+        </Button>
       </div>
-    </div>
+    </>
   );
 }
