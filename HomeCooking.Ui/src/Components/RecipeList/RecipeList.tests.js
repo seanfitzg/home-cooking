de55d4f37e7d203abff55128f893aa36602b5a28
Seán Fitzgerald <seanfitzg@gmail.com>
2021-06-15 23:14:20 +0100

    testing lib rename file

--- HomeCooking.Ui/src/Components/RecipeList/RecipeList.tests.js
+++
@@ -1,7 +0,0 @@
-import { render, fireEvent, waitFor, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import RecipeList from './RecipeList';
-
-test('loads the recipe list', async () => {
-  render(<RecipeList />);
-});
