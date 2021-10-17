310591979633e36b337b932b8e3d2c6a07567b5a
Seán Fitzgerald <seanfitzg@gmail.com>
2021-10-17 23:51:38 +0100

    Added home-cooking query service.

---
+++ HomeCooking.Queries/Controllers/NutritionDataController.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeCooking.Queries.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class NutritionDataController : ControllerBase
+    {
+        // GET: api/NutritionData
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/NutritionData/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/NutritionData
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/NutritionData/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/NutritionData/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
