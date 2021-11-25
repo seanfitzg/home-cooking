510cf060d6bcd263488cc641e3e7ea3b83eae431
Seán Fitzgerald <seanfitzg@gmail.com>
2021-11-25 19:46:43 +0000

    Added envoy and the shopping list service.

---
+++ HomeCooking.ShoppingList/Controllers/ListController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HomeCooking.ShoppingList.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ListController : ControllerBase
+    {
+        [HttpGet]
+        [ApiConventionMethod(typeof(DefaultApiConventions), 
+            nameof(DefaultApiConventions.Get))]       
+        public string Index()
+        {
+            return "OK";
+        }
+    }
+}
